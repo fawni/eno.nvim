0cd2b2334724db6d5a7509c990db46e443e5ebd7
fawn <fawn@rinici.de>
2024-05-26 05:17:54 +0300

    ✨ feature: init

---
+++ README.md
@@ -0,0 +1,20 @@
+# eno.nvim
+
+A Neovim plugin for editing [Eno](https://eno-lang.org/) files.
+
+## Installation
+
+Using [lazy.nvim](https://github.com/folke/lazy.nvim):
+```lua
+{
+    url = "git@codeberg.org:fawn/eno.nvim.git",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {},
+}
+```
+
+## License
+
+[Apache 2.0](LICENSE)
---
+++ queries/eno/highlights.scm
@@ -0,0 +1,18 @@
+(key) @property
+(value) @variable
+(entry (key) @keyword)
+(template) @string.special.symbol 
+
+(elementOperator) @operator
+(entryOperator) @operator
+(itemOperator) @operator
+(multilineFieldOperator) @operator
+(copyOperator) @operator
+(deepCopyOperator) @operator
+(directContinuationOperator) @operator
+(spacedContinuationOperator) @operator
+
+(escapeOperator) @string.escape
+(sectionOperator) @attribute.builtin
+
+(comment) @comment
